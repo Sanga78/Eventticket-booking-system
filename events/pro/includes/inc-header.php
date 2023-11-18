1dc2c4d08a8607a86883148949013076bea52eeb
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2023-11-18 16:22:45 +0300

    Update inc-header.php
    added fontawaesome links

--- events/pro/includes/inc-header.php
+++ events/pro/includes/inc-header.php
@@ -11,4 +11,10 @@ require_once '../constants.php';
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/signup.css">
+    <link rel="stylesheet" href="assets/css/signin.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous"> -->
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
 </head>
