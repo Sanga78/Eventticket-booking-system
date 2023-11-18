e63dc13a5803ba7a7529a54be530ba858198da1d
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2023-11-18 16:22:55 +0300

    Update signin.php
    updated signin

--- events/pro/signin.php
+++ events/pro/signin.php
@@ -59,44 +59,26 @@ alert("Access Denied.");
     }
 }
 ?>
-<div class="signup-page">
-    <div class="form">
-        <h2>Customer Panel</h2>
-        <br>
-        <form class="login-form" method="post" role="form" id="signup-form" autocomplete="off">
-            <!-- json response will be here -->
-            <div id="errorDiv"></div>
-            <!-- json response will be here -->
-
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label>Email Address</label>
-                    <input type="email" required name="email">
-                </div>
-            </div>
-
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" name="password" id="password">
-                    <span class="help-block" id="error"></span>
-                </div>
-            </div>
-
-
-
-            <div class="col-md-12">
-                <div class="form-group">
-                    <button type="submit" id="btn-signup">
-                        SIGN IN
-                    </button>
-                </div>
-            </div>
+<div class="login-page">
+    <center><h2>Customer Panel</h2></center>
+        <div class="form">
+    
+        <form>
+            <input type="email"name ="email" placeholder="&#xf003;  Email Address"/>
+            <input type="password" name="password" id="password" placeholder="&#xf023;  password"/>
+            <i class="fas fa-eye" onclick="show()"></i> 
+            <br>
+            <br>
+            <button>LOGIN</button>
             <p class="message">
-                <a href="#">.</a><br>
+              <a href="#">.</a><br>
             </p>
         </form>
-    </div>
+    
+        <form class="login-form">
+            <button type="button" onclick="window.location.href='individual_reg.php'">SIGN UP</button>
+        </form>
+        </div>
 </div>
 </div>
 <script src="assets/js/jquery-1.12.4-jquery.min.js"></script>
