5b487cbcb180b4370dca26ad9081e4557d6ea944
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2023-11-19 22:07:58 +0300

    Update session.php
    modified the session

--- events/pro/session.php
+++ events/pro/session.php
@@ -8,7 +8,7 @@
   $user_id = $_SESSION['user_id'];
   $email = $_SESSION['email'];
   if (!isset($conn)) require '../conn.php';
-  $exist = $conn->query("SELECT * FROM passenger WHERE id = '$user_id'")->num_rows;
+  $exist = $conn->query("SELECT * FROM customer WHERE id = '$user_id'")->num_rows;
   if ($exist != 1) {
     echo "<script>alert('You are not logged in!'); window.location='../';</script>";
     exit;
