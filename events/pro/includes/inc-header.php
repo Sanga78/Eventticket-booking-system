e94f8af975dec6eed230c874bf121124f481f8db
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2023-11-21 21:45:04 +0300

    Update inc-header.php
    added link to css

--- events/pro/includes/inc-header.php
+++ events/pro/includes/inc-header.php
@@ -10,6 +10,7 @@ require_once '../constants.php';
     <link href="assets/css/jquery-ui.css" rel="stylesheet" type="text/csss" />
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/intlTelInput.css">
     <link rel="stylesheet" href="assets/css/signup.css">
     <link rel="stylesheet" href="assets/css/signin.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous"> -->
