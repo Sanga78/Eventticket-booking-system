f57b8135cfa4e9d85970fec7da712aba8543207c
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2023-11-22 10:40:48 +0300

    Update adminsignin.php
    Updated admin sign in page

--- events/pro/adminsignin.php
+++ events/pro/adminsignin.php
@@ -53,41 +53,34 @@ alert("Access Denied.");
         <h2>Admin Sign In</h2>
         <br>
         <form class="login-form" method="post" role="form" id="signup-form" autocomplete="off">
-            <!-- json response will be here -->
-            <div id="errorDiv"></div>
-            <!-- json response will be here -->
-
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label>Email Address</label>
-                    <input type="text" required name="email">
-                </div>
-            </div>
-
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" name="password" id="password">
-                    <span class="help-block" id="error"></span>
-                </div>
-            </div>
-
-
-
-            <div class="col-md-12">
-                <div class="form-group">
-                    <button type="submit" id="btn-signup">
-                        SIGN IN
-                    </button>
-                </div>
-            </div>
+          <div id="errorDiv"></div>
+            <input type="email" required name ="email" placeholder="&#xf007;  Email Address"/>
+            <input type="password" name="password" id="password" placeholder="&#xf023;  password"/>
+            <i class="fas fa-eye" onclick="show()"></i> 
+            <br>
+            <br>
+            <button type="submit" id="btn-signup">LOGIN</button>
             <p class="message">
-                <a href="#">.</a><br>
+              <a href="#">.</a><br>
             </p>
         </form>
     </div>
 </div>
 </div>
+<script>
+      function show(){
+        var password = document.getElementById("password");
+        var icon = document.querySelector(".fas")
+  
+        // ========== Checking type of password ===========
+        if(password.type === "password"){
+          password.type = "text";
+        }
+        else {
+          password.type = "password";
+        }
+      };
+    </script>
 <script src="assets/js/jquery-1.12.4-jquery.min.js"></script>
 
 </body>
