f5506bab2032a19e8776c1944f09bf8eeed864ad
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-01-02 13:15:31 +0300

    Update reg.php
    updated the get  event from schedule function

--- events/pro/individual/reg.php
+++ events/pro/individual/reg.php
@@ -54,7 +54,7 @@ $me = $_SESSION['user_id'];
                                 }
                                 $id = $fetch['id']; ?><tr>
                                 <td><?php echo ++$sn; ?></td>
-                                <td><?php echo $fullname =  getRoutePath($fetch['route_id']);
+                                <td><?php echo $fullname =  getOrganizerName($fetch['organizer_id']);
                                         ?></td>
                                 <td><?php $array = getTotalBookByType($id);
                                         echo ($max_first = ($array['first'] - $array['first_booked'])), " Seat(s) Available for First Class" . "<hr/>" . ($max_second = ($array['second'] - $array['second_booked'])) . " Seat(s) Available for Second Class";
