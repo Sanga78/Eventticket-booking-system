53cad85ce2c1878ab4632052741b6532f97cdd9d
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-19 11:13:13 +0300

    Update inc-header.php
    updated fontawesome link

--- events/pro/includes/inc-header.php
+++ events/pro/includes/inc-header.php
@@ -13,7 +13,7 @@ require_once '../constants.php';
     <link rel="stylesheet" href="assets/css/intlTelInput.css">
     <link rel="stylesheet" href="assets/css/signup.css">
     <link rel="stylesheet" href="assets/css/signin.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
     <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
