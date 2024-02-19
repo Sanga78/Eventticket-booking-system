c88ef2c8c20bebc77d5cbd72f369c564f48c2f88
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-19 11:14:13 +0300

    Update index.php
    updated the index page

--- events/pro/organizer/index.php
+++ events/pro/organizer/index.php
@@ -70,7 +70,7 @@ if (!isset($file_access)) die("Direct File Access Denied");
 
                
     </div>
-                 <div class="col-md-3 col-sm-6 col-12">
+        <!-- <div class="col-md-3 col-sm-6 col-12">
             <div class="info-box bg-success">
                 <span class="info-box-icon"><i class="fa fa-dollar-sign"></i></span>
 
@@ -89,7 +89,7 @@ if (!isset($file_access)) die("Direct File Access Denied");
                 <!-- /.info-box-content -->
             </div>
             <!-- /.info-box -->
-        </div>
+        </div> -->
 
         <!-- /.col-md-6 -->
     </div>
