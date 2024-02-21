570f3aeddd199119f20e576b1e0a744051d548c5
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-21 22:53:35 +0300

    deletes
    deletes

--- events/MPESA/ResultURL.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  $callbackResponse = file_get_contents('php://input');
-
-  $logFile = "transaction_status.json";
-  $log = fopen($logFile, "a");
-  fwrite($log, $callbackResponse);
-  fclose($log);
