ea0fb78085d156562db964963117d1b2794530ca
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-22 22:06:17 +0300

    Update inc-nav.php
    updated navbar

--- events/pro/includes/inc-nav.php
+++ events/pro/includes/inc-nav.php
@@ -1,23 +1,12 @@
 <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-
-
-            </div>
-            <ul class="nav navbar-nav">
-                <li class="<?php echo $class == 'reg' ? 'active' : '' ?>">
-                    <a href="individual_reg.php">Sign Up</a>
-                </li>
-                <li class="<?php echo $class != 'reg' ? 'active' : '' ?>">
-
-                    <a href="signin.php">Sign In</a>
-
-                <li>
-                    <a href="../">Home</a>
-                </li>
-            </ul>
-        </div>
-
+    <nav class="navbar">
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+            <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
+            <!-- Menu-->
+                <ul class="menu">
+                    <li><a href="../">Home</a></li>
+                    <li class="<?php echo $class != 'reg' ? 'active' : '' ?>"><a href="signin.php">Sign In</a></li>
+                    <li class="<?php echo $class == 'reg' ? 'active' : '' ?>"><a href="individual_reg.php">Sign Up</a></li>
+                </ul>
     </nav>
 </body>
