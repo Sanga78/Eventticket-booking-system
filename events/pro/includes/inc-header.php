870f4d4816f1c15ce6830a9fdfffe81664a64617
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-22 22:05:58 +0300

    Update inc-header.php
    added a link to the  navbar styles

--- events/pro/includes/inc-header.php
+++ events/pro/includes/inc-header.php
@@ -7,9 +7,10 @@ require_once '../constants.php';
 
 <head>
     <title><?php echo SITE_NAME ?></title>
+    <link rel="stylesheet" type="text/css" href="assets/css/nav.css">
     <link href="assets/css/jquery-ui.css" rel="stylesheet" type="text/csss" />
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
     <link rel="stylesheet" href="assets/css/intlTelInput.css">
     <link rel="stylesheet" href="assets/css/signup.css">
     <link rel="stylesheet" href="assets/css/signin.css">
