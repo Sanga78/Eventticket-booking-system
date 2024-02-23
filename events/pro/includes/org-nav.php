399098681dd09dcaf4bf36e4dd1f5de05a6696d8
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-23 21:50:13 +0300

    Update org-nav.php
    updated the navbar

--- events/pro/includes/org-nav.php
+++ events/pro/includes/org-nav.php
@@ -1,12 +1,12 @@
 <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-
-
-            </div>
-            <ul class="nav navbar-nav">
-                <li class="<?php echo $class == 'reg' ? 'active' : '' ?>">
+    <nav class="navbar">
+    <img class="logo" id="logo" src="assets/img/logo.jpg" alt="Logo">
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+            <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
+            <!-- Menu-->
+                <ul class="menu">
+                    <li><a href="../">Home</a></li>
+                    <li class="<?php echo $class == 'reg' ? 'active' : '' ?>">
                     <a href="organizer_reg.php">Sign Up</a>
                 </li>
                 <li class="<?php echo $class != 'reg' ? 'active' : '' ?>">
@@ -14,9 +14,6 @@
                     <a href="organizer_signin.php">Sign In</a>
 
                 <li>
-                    <a href="../">Home</a>
-                </li>
-            </ul>
-        </div>
-
+                </ul>
     </nav>
+</body>
