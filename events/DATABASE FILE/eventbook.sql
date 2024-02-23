172213b3dcd7b149619dc921b38c5da5d5658463
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-23 22:00:46 +0300

    Update eventbook.sql
    updated database

--- events/DATABASE FILE/eventbook.sql
+++ events/DATABASE FILE/eventbook.sql
@@ -19,15 +19,6 @@ CREATE TABLE `availability` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `availability`
---
-
-INSERT INTO `availability` (`id`, `event`, `organizer`, `date`, `time`, `amount`, `status`) VALUES
-(1, 1, 1, '2024-03-20', '13:00:00', '1', 'available'),
-(2, 1, 2, '2024-01-22', '20:00:00', '2', 'not available'),
-(3, 2, 1, '2024-03-05', '13:00:00', '1', 'available');
-
---
 -- Table structure for table `seats`
 --
 
@@ -36,22 +27,6 @@ CREATE TABLE `seats` (
   `seat_booked` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `seats`
---
-
-INSERT INTO `seats` (`event_id`, `seat_booked`) VALUES
-('ABC0010', NULL),
-('BCC9999', NULL),
-('CAS3300', '16'),
-('LLL7699', NULL),
-('MMM9969', '2,15,6,18,12'),
-('MVL1000', '3'),
-('NBS4455', NULL),
-('RDH4255', '15'),
-('SSX6633', NULL),
-('TTH8888', NULL),
-('XYZ7890', NULL);
 
 -- --------------------------------------------------------
 
@@ -64,18 +39,6 @@ CREATE TABLE `event` (
   `first_seat` int(11) NOT NULL,
   `second_seat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `event`
---
-
-INSERT INTO `event` (`id`, `name`,`organizer_id`,`meeting_id`, `first_seat`, `second_seat`) VALUES
-(1, 'TECH FEST',1, 3, 30, 45),
-(2, 'EUSDA', 2, 5, 30, 45),
-(3, 'EUNCCU', 3, 4, 30, 45),
-(4, 'EUCOSA TRIP',1, 3, 30, 45),
-(5, 'EUSSA', 2, 5, 30, 45),
-(6, 'TRICKY FEST', 3, 4, 30, 45),
 
 -- --------------------------------------------------------
 
@@ -91,17 +54,6 @@ CREATE TABLE `booked` (
   `date` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `booked`
---
-
-INSERT INTO `booked` (`id`, `schedule_id`, `payment_id`, `user_id`, `code`, `class`, `no`, `seat`, `date`) VALUES
-(1, 5, 12, 4, '2024/002/1324', 'first', 1, 'F1', 'Tue, 20-Feb-2024 11:52:19 AM'),
-(2, 5, 15, 3, '2024/002/2645', 'first', 5, 'F02', 'Fri, 23-Feb-2024 12:48:38 PM'),
-(3, 6, 16, 1, '2024/003/1655', 'first', 8, 'F1 -F8', 'Sat, 9-Mar-2024 01:08:20 PM'),
-(4, 6, 1, 5, '2024/003/9146', 'second', 1, 'S1', 'Tue, 18-Mar-2024 01:09:22 PM'),
-(5, 8, 18, 2, '2024/003/1144', 'second', 8, 'S1 -S8', 'Mon, 31-Mar-2024 01:12:58 PM'),
-
 
 -- --------------------------------------------------------
 
@@ -116,18 +68,6 @@ CREATE TABLE `feedback` (
   `response` varchar(400) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `feedback`
---
-
-INSERT INTO `feedback` (`id`, `user_id`, `message`, `response`) VALUES
-(1, 3, 'This is a demo test.', NULL),
-(3, 6, 'Demo Test - 2', 'Are you sure that this is another test? '),
-(8, 4, 'This is a feedback text', NULL),
-(9, 6, 'Test Test Test Test Test', NULL),
-(11, 8, 'This is a demo test for feedback sections!!!', 'none');
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `customer`
@@ -143,18 +83,6 @@ CREATE TABLE `customer` (
   `loc` varchar(40) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `customer`
---
-
-INSERT INTO `customer` (`id`, `name`, `email`, `password`, `phone`, `address`, `loc`, `status`) VALUES
-(1, 'Customer One', 'cust1o@mail.com', '1f87051e29a6927b2e6651dfb9b66387', '0780100000', 'No. 20 Njoro', 'f3fc8566140434f0a3f47303c62d5146.jpg', 1),
-(2, 'Customer Two', 'pcust@mail.com', 'c3a19571f1271af5f27a9582377b7d4a', '1400000020', 'Njokerio', 'f3fc8566140434f0a3f47303c62d5146.jpg', 0),
-(3, 'Customer Three', 'cust3@mail.com', '1dd76b458af8df200a097c5b061df9b1', '9000001000', 'No. 589 Eldoret', 'f3fc8566140434f0a3f47303c62d5146.jpg', 1),
-(4, 'Customer Four', 'cust4@mail.com', 'd780455a563c7c5dbfb74a51785ad949', '0000010020', 'Nairobi', 'f3fc8566140434f0a3f47303c62d5146.jpg', 1),
-(5, 'Demo Account', 'demoaccount@mail.com', '5f4dcc3b5aa765d61d8327deb882cf99', '7800000000', '100 Egerton', '404a6378027a553d980b99162a5b4ce8.png', 1);
-(6, 'Kipkosgei Kelvin', 'kelvinkipkosgeisanga@gmail.com', '1234', '0111823379', '00232 Ahero', '404a6378027a553d980b99162a5b4ce8.png', 1);
 
 -- --------------------------------------------------------
 
@@ -173,18 +101,6 @@ CREATE TABLE `organizer` (
   `status` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `organizer`
---
-
-INSERT INTO `organizer` (`id`, `name`, `email`, `password`, `phone`, `address`, `loc`, `status`) VALUES
-(1, 'organizer One', 'pas1o@mail.com', '1f87051e29a6927b2e6651dfb9b66387', '0780100000', 'No. 20 Aiyeteju Street', 'f3fc8566140434f0a3f47303c62d5146.jpg', 1),
-(2, 'Adelabu Simbiat', 'jobowonubi@otrs.com', '1526755d438e395e551f229a484f8a1d', '3000002000', 'No. 30 Tanke Ilorin', 'f3fc8566140434f0a3f47303c62d5146.jpg', 1),
-(3, 'Organizer Two', 'pass2@mail.com', 'c3a19571f1271af5f27a9582377b7d4a', '1400000020', 'abrahamjasmine', 'f3fc8566140434f0a3f47303c62d5146.jpg', 0),
-(4, 'organizer Three', 'pass3@mail.com', '1dd76b458af8df200a097c5b061df9b1', '9000001000', 'No. 589 Ilorin', 'f3fc8566140434f0a3f47303c62d5146.jpg', 1),
-(5, 'organizer Four', 'pass4@mail.com', 'd780455a563c7c5dbfb74a51785ad949', '0000010020', 'Shagamu', 'f3fc8566140434f0a3f47303c62d5146.jpg', 1),
-(6, 'Test organizer', 'testpass@mail.com', 'abe1bcf64eb68c39847b962e8caadadf', '0000002000', 'Ilorin', 'f3fc8566140434f0a3f47303c62d5146.jpg', 1),
- --------------------------------------------------------
 
 --
 -- Table structure for table `payment`
@@ -199,19 +115,8 @@ CREATE TABLE `payment` (
   `date` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `payment`
---
 
-INSERT INTO `payment` (`id`, `customer_id`, `schedule_id`, `amount`, `ref`, `date`) VALUES
-(1, 5, 20, '150', '84JP4U5LKZ', 'Mon, 29-Feb-2024 11:36:57 PM'),
-(2, 4, 5, '500', 'oyki20masb', 'Tue, 03-Mar-2024 11:52:19 AM'),
-(3, 1, 18, '150', '3TVSHVBQII', 'Tue, 11-Mar-2024 04:10:29 PM'),
-(4, 6, 6, '1000', 'oyki20masb', 'Tue, 18-Mar-2024 11:52:19 AM'),
-(5, 2, 5, '300', '5gtnjnzclw', 'Tue, 25-Mar-2024 12:48:38 PM'),
-(6, 3, 6, '1500', 'dzwl1488r0', 'Tue, 01-Apr-2024 01:08:20 PM'),
-
--- --------------------------------------------------------
+-- -- --------------------------------------------------------
 
 --
 -- Table structure for table `meeting`
@@ -222,18 +127,6 @@ CREATE TABLE `meeting` (
   `venue` varchar(100) NOT NULL,
   `link` varchar(100) NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `meeting`
---
-
-INSERT INTO `meeting` (`id`, `venue`, `link`) VALUES
-(1, 'ELDORET','MEETINGLINK'),
-(2, 'NAIROBI','MEETINGLINK'),
-(3, 'NJORO','MEETINGLINK'),
-(4, 'NAKURU','MEETINGLINK'),
-(5, 'NAIROBI','MEETINGLINK'),
-(6, 'MOMBASA','MEETINGLINK'),
 
 
 -- --------------------------------------------------------
@@ -252,18 +145,6 @@ CREATE TABLE `schedule` (
   `second_fee` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `schedule`
---
-
-INSERT INTO `schedule` (`id`, `event_id`, `organizer_id`, `date`, `time`, `first_fee`, `second_fee`) VALUES
-(1, 6, 5, '13-10-2023', '18:30', 150, 100),
-(2, 1, 3, '14-10-2023', '18:30', 70, 50),
-(3, 4, 1, '15-10-2023', '18:30', 1000, 800),
-(4, 5, 2, '16-11-2023', '18:30', 500, 300),
-(5, 3, 4, '11-10-2023', '18:30', 500, 300),
-(6, 2, 6, '11-10-2023', '18:30', 1500, 1000),
--- --------------------------------------------------------
 
 
 --
@@ -301,14 +182,6 @@ ALTER TABLE `availability`
 ALTER TABLE `event`
   ADD PRIMARY KEY (`id`);
 
-
---
--- Indexes for table `seats`
---
-ALTER TABLE `seats`
-  ADD PRIMARY KEY (`id`);
-
-
 --
 -- Indexes for table `booked`
 --
@@ -330,6 +203,11 @@ ALTER TABLE `feedback`
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `organizer`
+--
+ALTER TABLE `organizer`
+  ADD PRIMARY KEY (`id`);
 --
 -- Indexes for table `payment`
 --
@@ -381,7 +259,7 @@ ALTER TABLE `customer`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 --
--- AUTO_INCREMENT for table `customer`
+-- AUTO_INCREMENT for table `organizer`
 --
 ALTER TABLE `organizer`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
@@ -432,7 +310,7 @@ ALTER TABLE `payment`
 -- Indexes for table `seats`
 --
 ALTER TABLE `seats`
-  ADD PRIMARY KEY (`event_no`);
+  ADD PRIMARY KEY (`event_id`);
 
 --
 -- Constraints for table `schedule`
@@ -441,6 +319,3 @@ ALTER TABLE `schedule`
   ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`),
   ADD CONSTRAINT `schedule_ibfk_2` FOREIGN KEY (`organizer_id`) REFERENCES `organizer` (`id`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
