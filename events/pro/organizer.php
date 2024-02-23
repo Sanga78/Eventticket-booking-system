ecb1f6fb6861e4e392388b8ed5a1c09f6340ec29
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-23 21:49:43 +0300

    Update organizer.php
    updated the alternative image for profile

--- events/pro/organizer.php
+++ events/pro/organizer.php
@@ -67,8 +67,10 @@ $fullname =  getOrganizerName($_SESSION['organizer_id'], $conn);
             <div class="sidebar">
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="images/trainlg.png" class="img-circle elevation-2" alt="User Image">
+                <div class="image">
+                        <img src="<?php
+                                    echo getOrgImage($_SESSION['user_id'], $conn);
+                                    ?>" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">Organizer</a>
