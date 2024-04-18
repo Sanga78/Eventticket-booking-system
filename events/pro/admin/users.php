ccec862a8daea7c4a2a789c6ef446d220a0da129
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-04-18 19:32:12 +0300

    Update users.php
    added print functionality

--- events/pro/admin/users.php
+++ events/pro/admin/users.php
@@ -23,10 +23,8 @@ if (isset($_GET['status'], $_GET['id'])) {
                 <div class="col-sm-12">
                     <div class="card card-success">
                         <div class="card-header">
-                            <h3 class="card-title">
-                                Registered Users</h3>
+                            <h3 class="card-title">Registered Users</h3>
                         </div>
-
                         <div class="card-body">
                             <div class="table-responsive">
 
@@ -91,295 +89,23 @@ if (isset($_GET['status'], $_GET['id'])) {
                 </div>
             </div>
         </div>
-</div>
-</div>
-</div>
-</div>
-</section>
+    </section>
 </div>
 
-<div class="modal fade" id="add">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content" align="center">
-            <div class="modal-header">
-                <h4 class="modal-title">Add New Schedule &#128649;
-                </h4>
+<center><button id="printpagebutton"  class="btn btn-primary" onClick="printpage()"><span class="glyphicon glyphicon-print"></span> &nbsp;Print</button></center>
 
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="" method="post">
-                    <div class="row">
-                        <div class="col-sm-6">
-                           Event: <select class="form-control" name="train_id" required id="">
-                                <option value="">Select Event</option>
-                                <?php
-                                $con = connect()->query("SELECT * FROM train");
-                                while ($row = $con->fetch_assoc()) {
-                                    echo "<option value='" . $row['id'] . "'>" . $row['name'] . "</option>";
-                                }
-                                ?>
-                            </select>
-
-                        </div>
-                        <div class="col-sm-6">
-                            Organizer : <select class="form-control" name="route_id" required id="">
-                                <option value="">Select Organizer</option>
-                                <?php
-                                $con = connect()->query("SELECT * FROM route");
-                                while ($row = $con->fetch_assoc()) {
-                                    echo "<option value='" . $row['id'] . "'>" . getRoutePath($row['id']) . "</option>";
-                                }
-                                ?>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            First Class Charge : <input class="form-control" type="number" name="first_fee" required
-                                id="">
-                        </div>
-                        <div class="col-sm-6">
-
-                            Second Class Charge : <input class="form-control" type="number" name="second_fee" required
-                                id="">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            Date : <input class="form-control" onchange="check(this.value)" type="date" name="date"
-                                required id="date">
-                        </div>
-                        <div class="col-sm-6">
-
-                            Time : <input class="form-control" type="time" name="time" required id="">
-                        </div>
-                    </div>
-                    <hr>
-                    <input type="submit" name="submit" class="btn btn-success" value="Add Schedule"></p>
-                </form>
-
-                <script>
-                function check(val) {
-                    val = new Date(val);
-                    var age = (Date.now() - val) / 31557600000;
-                    var formDate = document.getElementById('date');
-                    if (age > 0) {
-                        alert("Past/Current Date not allowed");
-                        formDate.value = "";
-                        return false;
-                    }
-                }
-                </script>
-
-            </div>
-
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
-
-
-<div class="modal fade" id="add2">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content" align="center">
-            <div class="modal-header">
-                <h4 class="modal-title">Add Range Schedule &#128649;
-                </h4>
-
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="" method="post">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            Event: <select class="form-control" name="train_id" required id="">
-                                <option value="">Select event</option>
-                                <?php
-                                $con = connect()->query("SELECT * FROM train");
-                                while ($row = $con->fetch_assoc()) {
-                                    echo "<option value='" . $row['id'] . "'>" . $row['name'] . "</option>";
-                                }
-                                ?>
-                            </select>
-
-                        </div>
-                        <div class="col-sm-6">
-                            Organizer : <select class="form-control" name="route_id" required id="">
-                                <option value="">Select organizer</option>
-                                <?php
-                                $con = connect()->query("SELECT * FROM route");
-                                while ($row = $con->fetch_assoc()) {
-                                    echo "<option value='" . $row['id'] . "'>" . getRoutePath($row['id']) . "</option>";
-                                }
-                                ?>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            First Class Charge : <input class="form-control" type="number" name="first_fee" required>
-                        </div>
-                        <div class="col-sm-6">
-
-                            Second Class Charge : <input class="form-control" type="number" name="second_fee" required>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            From Date : <input class="form-control" onchange="check(this.value)" type="date"
-                                name="from_date" required>
-                        </div>
-                        <div class="col-sm-6">
-                            End Date : <input class="form-control" onchange="check(this.value)" type="date"
-                                name="to_date" required>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6"> Every :
-                            <select class="form-control" name="every">
-                                <option value="Day">Day</option>
-                                <option value="Monday">Monday</option>
-                                <option value="Tuesday">Tuesday</option>
-                                <option value="Wednesday">Wednesday</option>
-                                <option value="Thursday">Thursday</option>
-                                <option value="Friday">Friday</option>
-                                <option value="Saturday">Saturday</option>
-                                <option value="Sunday">Sunday</option>
-                            </select>
-                        </div>
-                        <div class="col-sm-6">
-
-                            Time : <input class="form-control" type="time" name="time" required id="">
-                        </div>
-                    </div>
-                    <hr>
-                    <input type="submit" name="submit2" class="btn btn-success" value="Add Schedule"></p>
-                </form>
-
-                <script>
-                function check(val) {
-                    val = new Date(val);
-                    var age = (Date.now() - val) / 31557600000;
-                    var formDate = document.getElementById('date');
-                    if (age > 0) {
-                        alert("You are using a past/current date!");
-                        val.value = "";
-                        return false;
-                    }
-                }
-                </script>
-
-            </div>
-
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
-<?php
-
-if (isset($_POST['submit'])) {
-    $route_id = $_POST['route_id'];
-    $train_id = $_POST['train_id'];
-    $first_fee = $_POST['first_fee'];
-    $second_fee = $_POST['second_fee'];
-    $date = $_POST['date'];
-    $date = formatDate($date);
-    // die($date);
-    // $endDate = date('Y-m-d' ,strtotime( $data['automatic_until'] ));
-    $time = $_POST['time'];
-    if (!isset($route_id, $train_id, $first_fee, $second_fee, $date, $time)) {
-        alert("Fill Form Properly!");
-    } else {
-        $conn = connect();
-        $ins = $conn->prepare("INSERT INTO `schedule`(`train_id`, `route_id`, `date`, `time`, `first_fee`, `second_fee`) VALUES (?,?,?,?,?,?)");
-        $ins->bind_param("iissii", $train_id, $route_id, $date, $time, $first_fee, $second_fee);
-        $ins->execute();
-        alert("Schedule Added!");
-        load($_SERVER['PHP_SELF'] . "$me");
+<script>
+	 function printpage()
+	 {
+        //Get the print button and put it into a variable
+        var printButton = document.getElementById("printpagebutton");
+        //Set the print button visibility to 'hidden' 
+        printButton.style.visibility = 'hidden';
+        //Print the page content
+        window.print()
+        //Set the print button to 'visible' again 
+        //[Delete this line if you want it to stay hidden after printing]
+        printButton.style.visibility = 'visible';
+		window.print();
     }
-}
-
-
-if (isset($_POST['submit2'])) {
-    $route_id = $_POST['route_id'];
-    $train_id = $_POST['train_id'];
-    $first_fee = $_POST['first_fee'];
-    $second_fee = $_POST['second_fee'];
-    $from_date = $_POST['from_date'];
-    $to_date = $_POST['to_date'];
-    $every = $_POST['every'];
-
-    $time = $_POST['time'];
-    if (!isset($route_id, $train_id, $first_fee, $second_fee, $date, $time)) {
-        alert("Fill Form Properly!");
-    } else {
-
-
-        $from_date = formatDate($from_date);
-        $to_date = formatDate($to_date);
-        $startDate = $from_date;
-        $endDate = $to_date;
-        $conn = connect();
-        if ($every == 'Day') {
-            for ($i = strtotime($startDate); $i <= strtotime($endDate); $i = strtotime('+1 day', $i)) {
-                $date = date('d-m-Y', $i);
-                $ins = $conn->prepare("INSERT INTO `schedule`(`train_id`, `route_id`, `date`, `time`, `first_fee`, `second_fee`) VALUES (?,?,?,?,?,?)");
-                $ins->bind_param("iissii", $train_id, $route_id, $date, $time, $first_fee, $second_fee);
-                $ins->execute();
-            }
-        } else {
-            for ($i = strtotime($every, strtotime($startDate)); $i <= strtotime($endDate); $i = strtotime('+1 week', $i)) {
-                $date = date('d-m-Y', $i);
-
-                $ins = $conn->prepare("INSERT INTO `schedule`(`train_id`, `route_id`, `date`, `time`, `first_fee`, `second_fee`) VALUES (?,?,?,?,?,?)");
-                $ins->bind_param("iissii", $train_id, $route_id, $date, $time, $first_fee, $second_fee);
-                $ins->execute();
-            }
-        }
-
-
-        alert("Schedules Added!");
-        load($_SERVER['PHP_SELF'] . "$me");
-    }
-}
-
-
-if (isset($_POST['edit'])) {
-    $route_id = $_POST['route_id'];
-    $train_id = $_POST['train_id'];
-    $first_fee = $_POST['first_fee'];
-    $second_fee = $_POST['second_fee'];
-    $date = $_POST['date'];
-    $time = $_POST['time'];
-    $id = $_POST['id'];
-    if (!isset($route_id, $train_id, $first_fee, $second_fee, $date, $time)) {
-        alert("Fill Form Properly!");
-    } else {
-        $conn = connect();
-        $ins = $conn->prepare("UPDATE `schedule` SET `train_id`=?,`route_id`=?,`date`=?,`time`=?,`first_fee`=?,`second_fee`=? WHERE id = ?");
-        $ins->bind_param("iissiii", $train_id, $route_id, $date, $time, $first_fee, $second_fee, $id);
-        $ins->execute();
-        alert("Schedule Modified!");
-        load($_SERVER['PHP_SELF'] . "$me");
-    }
-}
-
-if (isset($_POST['del_train'])) {
-    $con = connect();
-    $conn = $con->query("DELETE FROM schedule WHERE id = '" . $_POST['del_train'] . "'");
-    if ($con->affected_rows < 1) {
-        alert("Schedule Could Not Be Deleted. This Route Has Been Tied To Another Data!");
-        load($_SERVER['PHP_SELF'] . "$me");
-    } else {
-        alert("Schedule Deleted!");
-        load($_SERVER['PHP_SELF'] . "$me");
-    }
-}
-?>
+</script>
